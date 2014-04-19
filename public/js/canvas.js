4d0f6b7b27ca0c0ce0fa1f255575aa486dc1f16d
Jack Lin <f24u23@berkeley.edu>
2014-04-19 00:47:39 -0700

    Implement share state canvas with socket.io - working

--- public/js/canvas.js
+++ public/js/canvas.js
@@ -1,12 +1,29 @@
 $(function(){
+  
+  var socket = io.connect();
 
   var colorPurple = "#cb3594";
   var colorGreen = "#659b41";
   var colorYellow = "#ffcf33";
   var colorBrown = "#986928";
-
   var curColor = colorPurple;
   var clickColor = new Array();
+  var paint;
+    
+  var clickX = new Array();
+  var clickY = new Array();
+  var clickDrag = new Array();
+
+
+  socket.on('current_draw', function(draw_aggregate) {
+    console.log(draw_aggregate);
+  });
+
+  socket.on('update_draw', function(position){
+    redraw_perimeter(position.posX, position.posY, position.drag);
+  });
+
+
 
 
   $('#canvas').mousedown(function(e){
@@ -14,28 +31,30 @@ $(function(){
     var mouseY = e.pageY - this.offsetTop;
     paint = true;
     addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
-      redraw();
-    });
+    redraw();
+    socket.emit('drawing', {posX: clickX, posY: clickY, drag: clickDrag});
+
+  });
 
   $('#canvas').mousemove(function(e){
     if(paint){
       addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
       redraw();
+      socket.emit('drawing', {posX: clickX, posY: clickY, drag: clickDrag});
     }
   });
 
   $('#canvas').mouseup(function(e){
+    //socket.emit('drawing', {posX: clickX, posY: clickY, drag: clickDrag});
     paint = false;
   });
 
   $('#canvas').mouseleave(function(e){
+    //socket.emit('drawing', {posX: clickX, posY: clickY, drag: clickDrag});
     paint = false;
   });
 
-  var clickX = new Array();
-  var clickY = new Array();
-  var clickDrag = new Array();
-  var paint;
+
 
   function addClick(x, y, dragging)
   {
@@ -62,13 +81,32 @@ $(function(){
       context.lineTo(clickX[i], clickY[i]);
       context.closePath();
       context.stroke();
-    }
-    for (var i = 0; i < clickX.length; i++)
-    {
-      console.log(clickX[i], clickY[i]);
+
     }
 
-  }  
+  }
+
+  function redraw_perimeter(posX, posY, drag){
+    context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+  
+    context.strokeStyle = "#df4b26";
+    context.lineJoin = "round";
+    context.lineWidth = 5;
+    for(var i=0; i < posX.length; i++) {    
+
+      context.beginPath();
+      if(drag[i] && i){
+        context.moveTo(posX[i-1], posY[i-1]);
+      }else{
+        context.moveTo(posX[i]-1, posY[i]);
+      }
+      context.lineTo(posX[i], posY[i]);
+      context.closePath();
+      context.stroke();
+
+    }
+  }
+
 
 });
 
